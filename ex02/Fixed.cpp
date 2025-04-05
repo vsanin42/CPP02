cf3da89f53f374b4d60071c7870748ec6074c145
Vladislav Sanin <vladislav.sanin.419@gmail.com>
2025-04-05 18:47:38 +0200

    doing ex02

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsanin <vsanin@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 15:15:59 by vsanin            #+#    #+#             */
+/*   Updated: 2025/04/05 18:32:42 by vsanin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+#include <ostream>
+
+Fixed::Fixed() : value(0) { std::cout << "Default constructor called" << "\n"; }
+Fixed::~Fixed() { std::cout << "Destructor called" << "\n"; }
+
+Fixed::Fixed(const Fixed& ref)
+{
+	std::cout << "Copy constructor called" << "\n";
+	*this = ref;
+}
+
+Fixed& Fixed::operator=(const Fixed& ref)
+{
+	std::cout << "Copy assignment operator called" << "\n";
+	if (this != &ref)
+		value = ref.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const { return (value); }
+void Fixed::setRawBits(int const raw) { value = raw; }
+
+/* -------------------------------------------------------------------------- */
+/*                                    ex01                                    */
+/* -------------------------------------------------------------------------- */
+
+Fixed::Fixed(const int inum)
+{
+	std::cout << "Int constructor called" << "\n";
+	value = inum << fraction_bits;
+}
+
+Fixed::Fixed(const float fnum)
+{
+	std::cout << "Float constructor called" << "\n";
+	value = roundf(fnum * (1 << fraction_bits));
+}
+
+int Fixed::toInt(void) const
+{
+	return (value >> fraction_bits);
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)value / (float)(1 << fraction_bits));
+}
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& num)
+{
+	stream << num.toFloat();
+	return (stream);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    ex02                                    */
+/* -------------------------------------------------------------------------- */
+
+bool Fixed::operator==(const Fixed& ref) const
+{
+	return ((*this).getRawBits() == ref.getRawBits());
+}
+
+bool Fixed::operator!=(const Fixed& ref) const
+{
+	return (!(*this == ref));
+}
+
+bool Fixed::operator<(const Fixed& ref) const
+{
+	return ((*this).getRawBits() < ref.getRawBits());
+}
+
+bool Fixed::operator>(const Fixed& ref) const
+{
+	return (!((*this) < ref) && !((*this) == ref));
+}
+
+bool Fixed::operator<=(const Fixed& ref) const
+{
+	return ((*this) < ref || (*this) == ref);
+}
+
+bool Fixed::operator>=(const Fixed& ref) const
+{
+	return (!((*this) < ref));
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsanin <vsanin@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 15:16:13 by vsanin            #+#    #+#             */
+/*   Updated: 2025/04/05 18:32:46 by vsanin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <ostream>
+
+class Fixed
+{
+	private:
+		int value;
+		static const int fraction_bits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed& ref);
+		~Fixed();
+		Fixed& operator=(const Fixed& ref);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+
+		Fixed(const int inum);
+		Fixed(const float fnum);
+		int toInt(void) const;
+		float toFloat(void) const;
+
+		bool operator==(const Fixed& ref) const;
+		bool operator!=(const Fixed& ref) const;
+		bool operator>(const Fixed& ref) const;
+		bool operator<(const Fixed& ref) const;
+		bool operator<=(const Fixed& ref) const;
+		bool operator>=(const Fixed& ref) const;
+};
+	
+std::ostream& operator<<(std::ostream& stream, const Fixed& num);
+
+#endif
